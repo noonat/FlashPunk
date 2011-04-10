a7c36e17312e64236db2e99b2ad30bafc090d47f
Nathan Ostgard <noonat@phuce.com>
2011-04-09 20:50:38 -0700

    Added fdb wrapper to Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -51,6 +51,11 @@ FileList['**/*.wav'].each do |wav_file|
   task :sounds => mp3_file
 end
 
+desc "Run an automated version of FDB"
+task :fdb do
+  Debugger.new.run
+end
+
 def compc(input_folders, output_file, opts={})
   mkdir_p File.dirname(output_file)
   args = compiler_args(opts[:command] || 'compc', opts)
@@ -131,3 +136,81 @@ def ffmpeg(input, output)
   end
 end
 
+class Debugger
+  def initialize
+    $stdout.sync = true
+    @line_triggers = {
+      "Player connected; session starting." => proc { trigger :connected },
+      "Player session terminated" => proc { trigger :connect },
+      "Failed to connect; session timed out." => proc { trigger :connect }
+    }
+    @triggers = {
+      "(fdb) " => proc { trigger :idle }
+    }
+    @read_buffer = ""
+    @write_buffer = ""
+    @state = :starting
+  end
+  
+  def run
+    open('|fdb', 'w+') do |handle|
+      @pipe = handle
+      while true do
+        IO.select [@pipe], [], [@pipe]
+        
+        # read
+        begin
+          data = @pipe.read_nonblock 1024
+          print data
+          @read_buffer += data
+        rescue Errno::EAGAIN, Errno::EWOULDBLOCK
+          nil
+        end
+        
+        # parse
+        until (i = @read_buffer.index("\n")) == nil
+          line, @read_buffer = @read_buffer.split("\n", 2)
+          # puts line
+          if (trigger = @line_triggers[line]) || (trigger = @triggers[line])
+            trigger.call 
+          end
+        end
+        if (trigger = @triggers[@read_buffer])
+          @read_buffer = ""
+          trigger.call
+        end
+        
+        # write
+        unless @write_buffer.empty?
+          length = @pipe.write_nonblock @write_buffer
+          @write_buffer.slice! length..-1 if length > 0
+        end
+      end
+    end
+  end
+  
+  def trigger(type)
+    case type
+    when :connect
+      write "run"
+      @state = :connecting
+    when :connected
+      @state = :connected
+    when :continue
+      write "continue"
+      @state = :running
+    when :idle
+      case @state
+      when :starting then trigger :connect
+      when :connected then trigger :continue
+      end
+    else
+      raise "Unknown trigger type #{type}"
+    end
+  end
+
+  def write(command)
+    print command, "\n"
+    @pipe.write command + "\n"
+  end
+end
